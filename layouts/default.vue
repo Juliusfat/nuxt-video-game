096163f0a61381f9525a82d5f4baf2cddedfb195
Cyrille P <Cyrille.paillard@free.fr>
2019-07-01 20:15:47 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="font-sans text-black antialiased">
+    <navigation></navigation>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation'
+import axios from 'axios'
+axios.defaults.headers.common['user-key'] = '41172b6051b977eb96d2f9170409e350'
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
